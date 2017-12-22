9944a02ef6f1b779e1dd42d34ac06f604bac7e69
Sean Liao <seankhliao@gmail.com>
2017-12-22 15:22:37 +0800

    add tree02.go

---
+++ tree02.go
@@ -0,0 +1,319 @@
+package main
+
+var tree02id = `tree02`
+
+var tree02name = `Dept. IM`
+var tree02cname = `資訊管理學系`
+
+var tree02desc = `This tree is for NTU IM`
+
+var tree02svg = `
+<svg xmlns="http://www.w3.org/2000/svg" id="Object" viewBox="0 0 1280 800">
+<defs>
+    <style>.cls-1{fill:#9acace;}.cls-10,.cls-2,.cls-3,.cls-8{font-size:15px;fill:#fff;font-family:Scada-Regular, Scada;}.cls-2,.cls-4{letter-spacing:0.05em;}.cls-3{letter-spacing:0.03em;}.cls-5{fill:#99acbb;}.cls-6{fill:#f99;}.cls-7{letter-spacing:0.02em;}.cls-8{letter-spacing:0.01em;}.cls-9{letter-spacing:0.03em;}.cls-11{fill:#f8da9e;}.cls-12{fill:#c33;}.cls-12,.cls-13,.cls-14,.cls-15{stroke:#fff;stroke-miterlimit:10;stroke-width:5px;}.cls-13{fill:#008490;}.cls-14{fill:#f93;}.cls-15{fill:#069;}</style>
+</defs>
+<title>SkillTree_UI_NTU IM tree_20171221</title>
+<g id="Green">
+    <g id="MSLTP">
+        <polygon class="cls-1" points="513 636 660 733 656 740 507 644 513 636"/>
+        <path class="cls-1" d="M543,581,660,726s-4,9-6,7S537,584,537,584Z"/>
+        <polygon class="cls-1" points="696 600 660 733 654 733 683 598 696 600"/>
+        <polygon class="cls-1" points="621 550 666 743 656 740 612 550 621 550"/>
+        <polygon class="cls-1" points="762 575 670 736 660 733 752 572 762 575"/>
+        <circle id="MSLTP_circle" class="cls-1" cx="660.5" cy="735.5" r="22.5"/>
+        <text class="cls-2" transform="translate(645.93 733.22) rotate(-0.9)">MSL<tspan x="6.3" y="18">TP</tspan>
+    </text>
+</g>
+<g id="Ccl2">
+    <circle id="Ccl2_circle" class="cls-1" cx="684.5" cy="600.5" r="22.5"/>
+    <text class="cls-3" transform="translate(674.69 598.15) rotate(-0.9)">C<tspan class="cls-4" x="8.17" y="0">cl</tspan>
+    <tspan class="cls-4">
+        <tspan x="0.15" y="18">(II)</tspan>
+    </tspan>
+</text>
+</g>
+<g id="Ccl1">
+    <polygon id="edge" class="cls-1" points="619.82 544.71 626 474 620 472 611.95 543.65 619.82 544.71"/>
+    <circle id="Ccl1_circle" class="cls-1" cx="615.5" cy="549.5" r="22.5"/>
+    <text class="cls-3" transform="translate(605.69 547.15) rotate(-0.9)">C<tspan class="cls-4" x="8.17" y="0">cl</tspan>
+    <tspan class="cls-4">
+        <tspan x="2.62" y="18">(I)</tspan>
+    </tspan>
+</text>
+</g>
+<g id="Stat2">
+    <circle id="Stat2_circle" class="cls-1" cx="506.5" cy="638.5" r="22.5"/>
+    <text class="cls-2" transform="translate(491.71 636.23) rotate(-0.9)">Stat<tspan x="5.14" y="18">(II)</tspan>
+</text>
+</g>
+<g id="Stat1">
+    <polygon id="edge-2" data-name="edge" class="cls-1" points="543.35 581.08 604 471 600 468 536.63 576.84 543.35 581.08"/>
+    <circle id="Stat1_circle" class="cls-1" cx="537.43" cy="583.64" r="22.5"/>
+    <text class="cls-2" transform="translate(522.82 581.06) rotate(1.71)">Stat<tspan x="7.61" y="18">(I)</tspan>
+</text>
+</g>
+<g id="MM">
+    <polygon id="edge-3" data-name="edge" class="cls-1" points="754.23 563.52 673.99 466.78 669.73 469.4 747.82 568.21 754.23 563.52"/>
+    <circle id="MM_circle" class="cls-1" cx="754.44" cy="569.96" r="22.5"/>
+    <text class="cls-2" transform="translate(741.27 575.12) rotate(0.92)">MM</text>
+</g>
+<g id="DM">
+    <polygon id="edge-4" data-name="edge" class="cls-1" points="675.27 536.46 650.46 469.95 644.18 470.72 667.7 538.88 675.27 536.46"/>
+    <circle id="DM_circle" class="cls-1" cx="673.42" cy="542.64" r="22.5"/>
+    <text class="cls-2" transform="translate(662.47 548.17) rotate(-2.45)">DM</text>
+</g>
+</g>
+<g id="Blue">
+    <g id="IMFP">
+        <polygon id="edge-5" data-name="edge" class="cls-5" points="552 247 603.84 353.86 596.62 357.89 543 252 552 247"/>
+        <circle id="IMFP_circle" class="cls-5" cx="546.65" cy="243.19" r="22.5"/>
+        <text class="cls-2" transform="translate(530.3 253.47) rotate(-14.57)">IMFP</text>
+    </g>
+    <g id="PoIM1">
+        <polygon id="edge-6" data-name="edge" class="cls-5" points="574 177 620 353 612 354 564 181 574 177"/>
+        <circle id="PoIM1_circle" class="cls-5" cx="566.31" cy="167.54" r="22.5"/>
+        <text class="cls-2" transform="translate(548.75 169.87) rotate(-14.57)">PoIM<tspan x="10.61" y="18">(I)</tspan>
+    </text>
+</g>
+<g id="PoIM2">
+    <polygon id="edge-7" data-name="edge" class="cls-5" points="554 117 566 153 558 155 543.65 118.34 554 117"/>
+    <circle id="PoIM2_circle" class="cls-5" cx="547.3" cy="109.54" r="22.5"/>
+    <text class="cls-2" transform="translate(529.73 111.87) rotate(-14.57)">PoIM<tspan x="8.14" y="18">(II)</tspan>
+</text>
+</g>
+<g id="MPD1">
+    <polygon id="edge-8" data-name="edge" class="cls-5" points="666 227 646 350 638 348 657 225 666 227"/>
+    <circle id="MPD1_circle" class="cls-5" cx="661.1" cy="226.55" r="22.5"/>
+    <text class="cls-2" transform="translate(645.27 225.05) rotate(2.05)">MPD<tspan x="8.55" y="18">(I)</tspan>
+</text>
+</g>
+<g id="MPD2">
+    <polygon id="edge-9" data-name="edge" class="cls-5" points="728 178 656 363 650 362 717 174 728 178"/>
+    <circle id="MPD2_circle" class="cls-5" cx="724.11" cy="175.54" r="22.5"/>
+    <text class="cls-2" transform="translate(709.27 173.05) rotate(2.05)">MPD<tspan x="6.08" y="18">(II)</tspan>
+</text>
+</g>
+<g id="SILP">
+    <circle id="SILP_circle" class="cls-5" cx="624.11" cy="95.52" r="22.5"/>
+    <text class="cls-2" transform="translate(609.02 101.05) rotate(2.05)">SILP</text>
+</g>
+</g>
+<g id="Red">
+    <g id="CN">
+        <polygon id="edge-10" data-name="edge" class="cls-6" points="537.73 495.11 581.54 459.27 576.49 453.13 533.61 488.32 537.73 495.11"/>
+        <circle id="CN_circle" class="cls-6" cx="531.29" cy="494.76" r="22.5"/>
+        <text class="cls-2" transform="matrix(1, 0.01, -0.01, 1, 521.62, 500.5)">CN</text>
+    </g>
+    <g id="ALG">
+        <polygon id="edge-11" data-name="edge" class="cls-6" points="389.91 498.16 443.57 480.15 441.03 472.62 388.47 490.35 389.91 498.16"/>
+        <circle id="ALG_circle" class="cls-6" cx="384.02" cy="495.56" r="22.5"/>
+        <text class="cls-2" transform="translate(374.52 504.9) rotate(-18.37)">A<tspan class="cls-7" x="9.3" y="0">L</tspan>
+        <tspan x="16.95" y="0">G</tspan>
+    </text>
+</g>
+<g id="DS">
+    <polygon id="edge-12" data-name="edge" class="cls-6" points="452.9 476.17 506.56 458.15 504.02 450.63 451.46 468.35 452.9 476.17"/>
+    <circle id="DS_circle" class="cls-6" cx="447.01" cy="473.56" r="22.5"/>
+    <text class="cls-2" transform="translate(441.46 481.59) rotate(-18.37)">DS</text>
+</g>
+<g id="PD">
+    <polygon id="edge-13" data-name="edge" class="cls-6" points="514 458.55 567.66 440.53 565.11 433.01 512.55 450.73 514 458.55"/>
+    <circle id="PD_circle" class="cls-6" cx="508.1" cy="455.94" r="22.5"/>
+    <text class="cls-2" transform="translate(502.13 464.11) rotate(-18.37)">PD</text>
+</g>
+<g id="DB">
+    <polygon id="edge-14" data-name="edge" class="cls-6" points="442.57 412.18 499.13 414.24 499.41 406.3 443.97 404.36 442.57 412.18"/>
+    <circle id="DB_circle" class="cls-6" cx="437.97" cy="407.66" r="22.5"/>
+    <text class="cls-2" transform="matrix(1, 0.04, -0.04, 1, 429.43, 413.2)">DB</text>
+</g>
+<g id="OS">
+    <polygon id="edge-15" data-name="edge" class="cls-6" points="507.44 415.94 564.03 416.85 564.14 408.9 508.68 408.09 507.44 415.94"/>
+    <circle id="OS_circle" class="cls-6" cx="502.75" cy="411.52" r="22.5"/>
+    <text class="cls-2" transform="matrix(1, 0.02, -0.02, 1, 494.73, 417.24)">OS</text>
+</g>
+<g id="COS">
+    <polygon id="edge-16" data-name="edge" class="cls-6" points="514.27 373.4 566.86 394.34 569.78 386.95 518.22 366.51 514.27 373.4"/>
+    <circle id="COS_circle" class="cls-6" cx="511.46" cy="367.6" r="22.5"/>
+    <text class="cls-8" transform="translate(499.12 374.31)">C<tspan class="cls-4" x="7.88" y="0">OS</tspan>
+</text>
+</g>
+<g id="SPM">
+    <circle id="SPM_circle" class="cls-6" cx="437.5" cy="243.5" r="22.5"/>
+    <text class="cls-2" transform="translate(422.64 249.31)">SPM</text>
+</g>
+<g id="SDM">
+    <polygon id="edge-17" data-name="edge" class="cls-6" points="488.33 292.89 531.57 329.41 536.69 323.33 494.26 287.6 488.33 292.89"/>
+    <circle id="SDM_circle" class="cls-6" cx="487.5" cy="286.5" r="22.5"/>
+    <text class="cls-2" transform="translate(472.49 292.31)">SDM</text>
+</g>
+<g id="SA">
+    <polygon id="edge-18" data-name="edge" class="cls-6" points="536.33 335.89 579.57 372.41 584.69 366.33 542.26 330.6 536.33 335.89"/>
+    <circle id="SA_circle" class="cls-6" cx="535.5" cy="329.5" r="22.5"/>
+    <text class="cls-2" transform="translate(527.28 335.31)">SA</text>
+</g>
+<g id="IR">
+    <circle id="IR_circle" class="cls-6" cx="396.5" cy="321.5" r="22.5"/>
+    <text class="cls-2" transform="translate(389.93 327.31)">IR</text>
+</g>
+<g id="GD">
+    <circle id="GD_circle" class="cls-6" cx="365.5" cy="235.5" r="22.5"/>
+    <text class="cls-2" transform="translate(356.72 241.31)">GD</text>
+</g>
+<g id="_3DP" data-name="3DP">
+    <circle id="_3DP_circle" data-name="3DP_circle" class="cls-6" cx="331.5" cy="297.5" r="22.5"/>
+    <text class="cls-2" transform="translate(318.59 303.31)">3DP</text>
+</g>
+<g id="BDDA">
+    <circle id="BDDA_circle" class="cls-6" cx="315.5" cy="367.5" r="22.5"/>
+    <text class="cls-2" transform="translate(297.38 373.31)">BD<tspan class="cls-9" x="18.9" y="0">D</tspan>
+    <tspan x="28.13" y="0">A</tspan>
+</text>
+</g>
+<g id="_3DCR" data-name="3DCR">
+    <circle id="_3DCR_circle" data-name="3DCR_circle" class="cls-6" cx="308.5" cy="445.5" r="22.5"/>
+    <text class="cls-2" transform="translate(291.28 451.31)">3DCR</text>
+</g>
+<g id="IS">
+    <circle id="IS_circle" class="cls-6" cx="424.5" cy="574.5" r="22.5"/>
+    <text class="cls-2" transform="translate(418.46 580.31)">IS</text>
+</g>
+<g id="ToC">
+    <circle id="ToC_circle" class="cls-6" cx="323.5" cy="517.5" r="22.5"/>
+    <text class="cls-10" transform="translate(311.91 523.31)">T<tspan class="cls-4" x="7.2" y="0">oC</tspan>
+</text>
+</g>
+<g id="PL">
+    <circle id="PL_circle" class="cls-6" cx="479.5" cy="535.5" r="22.5"/>
+    <text class="cls-2" transform="translate(471.43 541.31)">PL</text>
+</g>
+<g id="IE">
+    <circle id="IE_circle" class="cls-6" cx="345.5" cy="572.5" r="22.5"/>
+    <text class="cls-2" transform="translate(339.42 578.31)">IE</text>
+</g>
+<g id="HCI">
+    <circle id="HCI_circle" class="cls-6" cx="381.5" cy="619.5" r="22.5"/>
+    <text class="cls-2" transform="translate(370.28 625.31)">HCI</text>
+</g>
+</g>
+<g id="Yellow">
+    <g id="FM">
+        <polygon id="edge-19" data-name="edge" class="cls-11" points="811.82 241.11 758.58 260.33 761.29 267.8 813.44 248.89 811.82 241.11"/>
+        <circle id="FM_circle" class="cls-11" cx="820.5" cy="238.5" r="22.5"/>
+        <text class="cls-2" transform="translate(807.63 241.52) rotate(9.64)">FM</text>
+        <polygon id="edge-20" data-name="edge" class="cls-11" points="810.72 243.7 796.73 298.54 804.43 300.49 818.07 246.72 810.72 243.7"/>
+    </g>
+    <g id="MIS">
+        <polygon id="edge-21" data-name="edge" class="cls-11" points="709.39 317.37 672.39 360.2 678.41 365.39 714.61 323.36 709.39 317.37"/>
+        <circle id="MIS_circle" class="cls-11" cx="715.79" cy="316.61" r="22.5"/>
+        <text class="cls-2" transform="translate(700.44 319.21) rotate(9.64)">MIS</text>
+    </g>
+    <g id="Acct1">
+        <polygon id="edge-22" data-name="edge" class="cls-11" points="784.12 303.57 687 373 690 377 787.51 310.75 784.12 303.57"/>
+        <circle id="Acct1_circle" class="cls-11" cx="790.48" cy="304.58" r="22.5"/>
+        <text class="cls-2" transform="translate(774.33 303.24) rotate(-0.73)">Acct<tspan x="8.1" y="18">(I)</tspan>
+    </text>
+</g>
+<g id="Acct2">
+    <circle id="Acct2_circle" class="cls-11" cx="754.49" cy="267.57" r="22.5"/>
+    <text class="cls-2" transform="translate(738.34 266.22) rotate(-0.73)">Acct<tspan x="5.63" y="18">(II)</tspan>
+</text>
+</g>
+<g id="Eco1">
+    <polygon id="edge-23" data-name="edge" class="cls-11" points="753.14 373.81 699.11 390.68 701.49 398.26 754.42 381.66 753.14 373.81"/>
+    <circle id="Eco1_circle" class="cls-11" cx="758.98" cy="376.55" r="22.5"/>
+    <text class="cls-2" transform="translate(747.35 373.67) rotate(2.94)">Eco<tspan x="4.84" y="18">(I)</tspan>
+</text>
+</g>
+<g id="SM">
+    <polygon id="edge-24" data-name="edge" class="cls-11" points="880.14 387.71 761.83 422.18 763.55 429.04 881.28 395.57 880.14 387.71"/>
+    <circle id="SM_circle" class="cls-11" cx="885.93" cy="390.54" r="22.5"/>
+    <text class="cls-2" transform="translate(876.13 396.57) rotate(-2.73)">SM</text>
+</g>
+<g id="MtM">
+    <polygon id="edge-25" data-name="edge" class="cls-11" points="873.2 490.47 765.76 430.12 762.06 436.15 868.38 496.79 873.2 490.47"/>
+    <circle id="MtM_circle" class="cls-11" cx="875.23" cy="496.59" r="22.5"/>
+    <text class="cls-2" transform="translate(858.22 502.33)">MtM</text>
+</g>
+<g id="OB">
+    <polygon id="edge-26" data-name="edge" class="cls-11" points="884.57 440.73 762 428 761 435 882.66 448.44 884.57 440.73"/>
+    <circle id="OB_circle" class="cls-11" cx="888.86" cy="445.54" r="22.5"/>
+    <text class="cls-2" transform="translate(879.68 451.44) rotate(-1)">OB</text>
+</g>
+<g id="Mng">
+    <polygon id="edge-27" data-name="edge" class="cls-11" points="755.57 422.73 699.26 416.98 698.47 424.88 753.66 430.44 755.57 422.73"/>
+    <circle id="Mng_circle" class="cls-11" cx="759.86" cy="427.54" r="22.5"/>
+    <text class="cls-2" transform="translate(744.72 433.54) rotate(-1)">Mng</text>
+</g>
+<g id="OM">
+    <polygon id="edge-28" data-name="edge" class="cls-11" points="805.95 501.66 754.54 477.98 751.23 485.2 801.64 508.34 805.95 501.66"/>
+    <circle id="OM_circle" class="cls-11" cx="808.45" cy="507.6" r="22.5"/>
+    <text class="cls-2" transform="translate(797.02 514.07) rotate(-3.56)">OM</text>
+</g>
+<g id="OR">
+    <polygon id="edge-29" data-name="edge" class="cls-11" points="744.95 469.68 693.54 446 690.23 453.22 740.65 476.36 744.95 469.68"/>
+    <circle id="OR_circle" class="cls-11" cx="747.45" cy="475.62" r="22.5"/>
+    <text class="cls-2" transform="translate(738.01 481.96) rotate(-3.56)">OR</text>
+</g>
+<g id="FM-2" data-name="FM">
+    <circle id="FM_circle-2" data-name="FM_circle" class="cls-11" cx="946.5" cy="315.5" r="22.5"/>
+    <text class="cls-2" transform="matrix(1, -0.01, 0.01, 1, 931.26, 321.58)">ISIM</text>
+</g>
+</g>
+<g id="Center">
+    <path class="cls-12" d="M582.23,423.71a50,50,0,0,1,18.49-47.39l-11-13.44a64.27,64.27,0,0,0-8.43,8.23,67.58,67.58,0,0,0,7.11,95.32l10.87-12.62A49.82,49.82,0,0,1,582.23,423.71Z"/>
+    <path class="cls-13" d="M639.63,465a49.91,49.91,0,0,1-40.35-11.19l-10.87,12.62a63.28,63.28,0,0,0,9.56,6.88,67.6,67.6,0,0,0,92.65-23.56L675,440.46A49.89,49.89,0,0,1,639.63,465Z"/>
+    <path class="cls-14" d="M656.44,352,650,369.15a50,50,0,0,1,25,71.31l15.63,9.29a64,64,0,0,0,5.11-10.61A67.58,67.58,0,0,0,656.44,352Z"/>
+    <path class="cls-15" d="M623.53,366.31A49.79,49.79,0,0,1,650,369.15L656.44,352c-23.25-8.81-47.46-4.86-66.71,10.88l11,13.44A49.73,49.73,0,0,1,623.53,366.31Z"/>
+</g>
+</svg>
+`
+
+var tree02state = `
+{
+	"PD":{"CName":"程式設計","pre_course":[],"status":0},
+	"DS_APD":{"CName":"資料結構與進階程式設計","pre_course":["PD"],"status":0},
+	"PL":{"CName":"程式語言","pre_course":["PD"],"status":0},
+	"ToC":{"CName":"計算理論","pre_course":["Algorithm"],"status":0},
+	"CN_A":{"CName":"網路技術與應用","pre_course":[],"status":0},
+	"IS":{"CName":"資訊安全","pre_course":["CN_A","OS"],"status":0},
+	"OS":{"CName":"作業系統","pre_course":[],"status":0},
+	"DB":{"CName":"資料庫管理","pre_course":["OS"],"status":0},
+	"COS":{"CName":"計算機組織與結構","pre_course":[],"status":0},
+	"SA":{"CName":"系統分析與設計","pre_course":[],"status":0},
+	"SDM":{"CName":"軟體開發方法","pre_course":["SA"],"status":0},
+	"SPM":{"CName":"軟體專案管理","pre_course":[],"status":0},
+	"IR":{"CName":"資訊檢索與文字探勘導論","pre_course":[],"status":0},
+	"BD_BA":{"CName":"大數據與商業分析","pre_course":[],"status":0},
+	"GD":{"CName":"遊戲設計","pre_course":[],"status":0},
+	"3DCIR":{"CName":"3D創作實際","pre_course":[],"status":0},
+	"3DP":{"CName":"3D程式設計","pre_course":[],"status":0},
+	"IE":{"CName":"資訊經濟","pre_course":[],"status":0},
+	"HCI":{"CName":"人機互動","pre_course":[],"status":0},
+	"Algorithm":{"CName":"演算法","pre_course":["DS_APD"],"status":0},
+	"Calculus_a1":{"CName":"微積分甲上","pre_course":[],"status":0},
+	"Calculus_a2":{"CName":"微積分甲下","pre_course":["Calculus_a1"],"status":0},
+	"Statistics_I1":{"CName":"統計學一上","pre_course":[],"status":0},
+	"Statistics_I2":{"CName":"統計學一下","pre_course":["Statistics_I1"],"status":0},
+	"MM":{"CName":"管理數學","pre_course":[],"status":0},
+	"DM":{"CName":"離散數學","pre_course":[],"status":0},
+	"MSL_TP":{"CName":"統計學習初論","pre_course":["Calculus_a1","Calculus_a2","Statistics_I1","Statistics_I2","MM"],"status":0},
+	"Finance_1":{"CName":"會計學甲一上","pre_course":[],"status":0},
+	"Finance_2":{"CName":"會計學甲一下","pre_course":["Finance_1"],"status":0},
+	"Economics_1":{"CName":"經濟學一","pre_course":[],"status":0},
+	"Management":{"CName":"管理學","pre_course":[],"status":0},
+	"OR":{"CName":"作業研究","pre_course":[],"status":0},
+	"MIS":{"CName":"資訊管理導論","pre_course":[],"status":0},
+	"ISIM":{"CName":"資訊服務創新管理","pre_course":[],"status":0},
+	"FM":{"CName":"財務管理","pre_course":["Economics_1","Finance_1","Finance_2"],"status":0},
+	"SM":{"CName":"策略管理","pre_course":["Management"],"status":0},
+	"OB":{"CName":"組織行為","pre_course":["Management"],"status":0},
+	"MkM":{"CName":"行銷管理","pre_course":["Management"],"status":0},
+	"OM":{"CName":"作業管理","pre_course":["OR"],"status":0},
+	"IMFP":{"CName":"資管新生專題","pre_course":[],"status":0},
+	"PoIM_1":{"CName":"資管專題一","pre_course":[],"status":0},
+	"PoIM_2":{"CName":"資管專題二","pre_course":["PoIM_1"],"status":0},
+	"MPD_1":{"CName":"資管專題討論一","pre_course":[],"status":0},
+	"MPD_2":{"CName":"資管專題討論二","pre_course":[],"status":0},
+	"SILP":{"CName":"資訊法與資訊政策專題","pre_course":[],"status":0}
+}
+`
